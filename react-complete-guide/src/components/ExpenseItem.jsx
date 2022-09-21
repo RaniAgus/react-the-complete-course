5b94269eeb736fdbc2df231d6b1555b2c33dafdc
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-21 11:46:59 -0300

    38 - adding normal js logic to components

--- react-complete-guide/src/components/ExpenseItem.jsx
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -1,9 +1,17 @@
 import './ExpenseItem.css'
 
 const ExpenseItem = ({ expense }) => {
+  const month = expense.date.toLocaleString('en-US', { month: 'long' });
+  const day = expense.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = expense.date.getFullYear();
+
   return (
     <div className='expense-item'>
-      <div>{expense.date.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{expense.title}</h2>
         <div className='expense-item__price'>${expense.amount}</div>
