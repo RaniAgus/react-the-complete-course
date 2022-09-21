f052abd4a942b5fb50be795230b0913a7c82688f
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-21 11:30:16 -0300

    36 - outputting dynamic data and working with expressions in jsx

--- react-complete-guide/src/components/ExpenseItem.jsx
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -1,12 +1,18 @@
 import './ExpenseItem.css'
 
 const ExpenseItem = () => {
+  const expense = {
+    date: new Date(2021, 2, 28),
+    title: 'Car Insurance',
+    amount: 294.67,
+  };
+
   return (
     <div className='expense-item'>
-      <div>March 28th 2021</div>
+      <div>{expense.date.toDateString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{expense.title}</h2>
+        <div className='expense-item__price'>${expense.amount}</div>
       </div>
     </div>
   )
