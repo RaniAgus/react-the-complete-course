f010e34cf490b8b12517e00abb0c7f6fe1ba6dc6
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-23 11:31:23 -0300

    54 - working with multiple states

--- react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    // Siempre por defecto se recibe un string, aunque sea number o Date
+    setAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
   };
 
   return (
@@ -14,11 +29,13 @@ const ExpenseForm = () => {
         </div>
         <div className='new-expense__control'>
           <label htmlFor='amount'>Amount</label>
-          <input type='number' id='amount' />
+          <input type='number' id='amount' onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label htmlFor='date'>Date</label>
-          <input type='date' id='date' min='2019-01-01' max='2022-12-31' />
+          <input type='date' id='date'
+                 min='2019-01-01' max='2022-12-31'
+                 onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
