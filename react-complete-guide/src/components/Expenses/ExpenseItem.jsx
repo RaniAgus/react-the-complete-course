98eaea490920c5841dba41b357eeb189307cd450
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-23 10:58:35 -0300

    49 - working with state

--- react-complete-guide/src/components/Expenses/ExpenseItem.jsx
+++ react-complete-guide/src/components/Expenses/ExpenseItem.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ expense }) => {
+  // los hooks solo deben ser invocados dentro de un componente
+  // NO invocar dentro de una nested function
+  const [title, setTitle] = useState(expense.title);
+
   const changeTitleHandler = () => {
-    console.log('Clicked!');
+    setTitle('Updated!');
   };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={expense.date}/>
       <div className='expense-item__description'>
-        <h2>{expense.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${expense.amount}</div>
       </div>
       <button onClick={changeTitleHandler}>Change title</button>
