160cb1d390048f3f280084e62c5d2f05abc5c397
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-26 11:32:54 -0300

    70 - adding dynamic styles

--- react-complete-guide/src/components/Chart/ChartBar.jsx
+++ react-complete-guide/src/components/Chart/ChartBar.jsx
@@ -1,8 +1,18 @@
 import './ChartBar.css';
 
 const ChartBar = ({ label, value, maxValue }) => {
+  const barFillHeight = maxValue > 0 ? Math.round(value / maxValue) * 100 : 0;
+
   return (
-    <div>ChartBar</div>
+    <div className='chart-bar'>
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: `${barFillHeight}%` }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{label}</div>
+    </div>
   );
 };
 
