230c9204e3f870c4fcde20d16027ac5d5aeca139
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-26 09:51:55 -0300

    assignment 2 - working with events and state

--- react-complete-guide/src/components/Expenses/Expenses.jsx
+++ react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -1,11 +1,20 @@
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 import './Expenses.css';
+import { useState } from 'react';
 
 const Expenses = ({ expenses }) => {
+  const [selectedYear, setSelectedYear] = useState(2020);
+
+  const filterExpensesHandler = (year) => {
+    setSelectedYear(year);
+  }
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selectedYear={selectedYear} onFilterExpenses={filterExpensesHandler}/>
       <ExpenseItem expense={expenses[0]}></ExpenseItem>
       <ExpenseItem expense={expenses[1]}></ExpenseItem>
       <ExpenseItem expense={expenses[2]}></ExpenseItem>
