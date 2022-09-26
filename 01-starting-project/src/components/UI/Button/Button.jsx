b1d7f366d791b7a1f806f05b46b22faadfb7c0bc
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-26 13:22:23 -0300

    80 - using css modules

--- 01-starting-project/src/components/UI/Button/Button.jsx
+++ 01-starting-project/src/components/UI/Button/Button.jsx
@@ -1,31 +1,14 @@
-import styled from 'styled-components';
+// Este import permite usar css modules para que los estilos no se pisen entre
+// componentes. Se importa como un objeto, por ejemplo:
+// ".button { ... }" se convierte en un atributo styles.button
+import styles from './Button.module.css';
 
-// El package se encarga de redirigir todos los eventos
-// (onClick, onChange, etc)
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-
-  @media (min-width: 768px) {
-    width: auto;
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
